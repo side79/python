5411ed5b7faa144f28b4f47cdf1298dc8a6ec0fc
pyadm <wolf_7991@mail.ru>
2024-02-05 22:38:36 +0300

    Test

---
+++ lesson3.py
@@ -0,0 +1,15 @@
+a = [1, 2, 3]
+b = [1, 2, 3]
+c = a
+
+
+print("id of a:", id(a))
+print("id of b:", id(b))
+print("id of c:", id(c))
+
+
+print("a == b:", a == b)
+print("a is b:", a is b)
+
+print("a == c:", a == c)
+print("a is c:", a is c)
