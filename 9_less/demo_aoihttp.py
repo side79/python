5faa18f57de7c203400808141fc52970ff8f64f7
pyadm <wolf_7991@mail.ru>
2024-11-25 16:38:13 +0300

    async

--- 9_less/demo_aoihttp.py
+++ 9_less/demo_aoihttp.py
@@ -1,4 +1,5 @@
 import asyncio
+from asyncio import CancelledError
 from dataclasses import dataclass
 from aiohttp import ClientSession
 from loguru import logger
@@ -14,13 +15,42 @@ SERVICES = [
     Service("ip-api","http://ip-api.com/json", "query"),
 ]
 
+async def fetch(session: ClientSession, url: str) -> dict:
+    async with session.get(url) as response:
+        return await response.json()
+    
+
 async def fetch_ip(service: Service) -> str:
-    """_summary_
+   
+    async with ClientSession() as session:
+        result = await fetch(session, service.url)
+    logger.info("Got result for {}, result {}", service.name, result)
+    return  result[service.ip_field]
 
-    Args:
-        service (Service): _description_
+async def get_my_ip():
+    cors = [asyncio.create_task(fetch_ip(s)) for s in SERVICES]
+    done, pading = await asyncio.wait(
+        cors,
+        timeout=3,
+        return_when=asyncio.FIRST_COMPLETED
+    )
+    
+    for t in pading:
+        t.cancel()
+        
+    my_ip = None
+    
+    for t in done:
+        my_ip = t.result()
+        break
+    else:
+        logger.warning("No result")
+        
+    logger.info("Got my ip {}", my_ip)
 
-    Returns:
-        str: _description_
-    """
-    my_ip = "not found"
+
+def run_main():
+    asyncio.run(get_my_ip())
+        
+if __name__ == "__main__":
+    run_main()
