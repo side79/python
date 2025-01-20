1eb7498d217ff004ec34f6252228aa13cb498147
pyadm <wolf_7991@mail.ru>
2025-01-20 21:58:20 +0300

    many to many

--- 11_less/orm4.session_and_relations.py
+++ 11_less/orm4.session_and_relations.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime, ForeignKey, Table
 from sqlalchemy.orm import declarative_base, sessionmaker, scoped_session, Relationship
 from datetime import datetime
 
@@ -8,72 +8,88 @@ Base = declarative_base()
 session_factory = sessionmaker(bind=engine)
 Session = scoped_session(session_factory)
 
+post_tags_table = Table(
+    "posts_table",
+    Base.metadata,
+    Column("post_id", Integer, ForeignKey("posts.id"), primary_key=True),
+    Column("tag_id", Integer, ForeignKey("tags.id"), primary_key=True),
+)
+
 
 class User(Base):
     __tablename__ = "users"
-    
+
     id = Column(Integer, primary_key=True)
     username = Column(String(32), unique=True)
     is_staff = Column(Boolean, default=False)
     created_at = Column(DateTime, default=datetime.now)
-    
+
     posts = Relationship("Post", back_populates="author")
-    
+
     def __str__(self):
         return f"{self.__class__.__name__}(id={self.id}, username={self.username!r})"
-    
+
+
+class Tag(Base):  # Исправлено название класса
+    __tablename__ = "tags"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(32), unique=True, nullable=False)
+
+    posts = Relationship("Post", secondary=post_tags_table, back_populates="tags")  # Указан правильный класс
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, name={self.name})"
+
+    def __repr__(self):
+        return str(self)
+
+
 class Post(Base):
     __tablename__ = "posts"
 
     id = Column(Integer, primary_key=True)
     title = Column(String(256), nullable=False)
-    author_id = Column(Integer, ForeignKey(User.id), nullable=False) #or author_id = Column(Integer, ForeignKey("users")) if model(class) not show, where "users" is table name
+    author_id = Column(Integer, ForeignKey(User.id), nullable=False)
 
     author = Relationship(User, back_populates="posts")
-    
+    tags = Relationship("Tag", secondary=post_tags_table, back_populates="posts")  # Указан правильный класс
+
     def __str__(self):
         return f"{self.__class__.__name__}(id={self.id}, title={self.title!r}, author={self.author})"
-    
+
     def __repr__(self):
         return str(self)
 
 
-def create_user(username:str) -> User:
-    u = User(username="sam")
-    
+def create_user(username: str) -> User:
+    u = User(username=username)
+
     print("id before:", u.id)
     session.add(u)
     session.commit()
     print("id after:", u.id)
-    
-    return User
 
-def author_posts(title:str,) -> Post:
-    user: User = session.query(User).filter_by(username="sam").one()
-    print(user)
-    
-    #post = Post(title = "First post!", author=user)
-    #session.add(post)
-    #session.commit()
-    #print(post)
-    #print(user.posts)
-    
-    post = Post(title = "Second post!", author=user)
-    
-    session.add(post)
-    
-    user.posts.append(post)
-    session.commit()
-    
-    print(user.posts)
-    
-    
-    return Post
+    return u
+
 
 if __name__ == "__main__":
-    #Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
     session = Session()
     #u = create_user("sam")
-    
-    
+    user = session.query(User).filter_by(username="sam").one()
+    user.is_staff = True
+
+    tags = [Tag(name=name) for name in ("news", "flask", "django", "python")]
+    post = Post(title="News Flask vs Django", author=user)
+    post.tags.extend(tags)
+
+    session.add(post)
+    session.commit()
+
+    print(post, post.tags)
+
+    for tag in tags:
+        print(tag, tag.posts)
+
     session.close()
