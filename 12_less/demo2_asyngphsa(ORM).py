bfdcb07fd9c709a82ee43c605817665fbf68b1dd
pyadm <wolf_7991@mail.ru>
2025-01-22 23:34:12 +0300

    asyng(ORM)

---
+++ 12_less/demo2_asyngphsa(ORM).py
@@ -0,0 +1,42 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import MetaData, Table, Column, Integer, String, Date, select
+
+
+metadata = MetaData()
+
+users_table = Table(
+    "users",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String),
+    Column("surname", String),
+    Column("birth_date", Date, nullable=False),
+)
+
+DATABASE_URL = "postgresql+asyncpg://postgres:123@localhost/postgres"
+
+async def main():
+    # Создаем асинхронный движок и сессию
+    engine = create_async_engine(DATABASE_URL, echo=True)
+    async_session = sessionmaker(
+        bind=engine, class_=AsyncSession, expire_on_commit=False
+    )
+
+    try:
+        async with async_session() as session:
+            # Выполнение запроса
+            query = select(users_table)
+            result = await session.execute(query)
+            rows = result.fetchall()
+
+            for row in rows:
+                print(row)
+    finally:
+        # Закрываем движок
+        await engine.dispose()
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
